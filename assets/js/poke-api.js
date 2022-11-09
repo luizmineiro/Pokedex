66bcf90f45c7a17d2bcbc246064a1ec013d16922
João Luiz <jluizmineiro@gmail.com>
2022-11-09 06:40:47 -0300

    update

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,23 +1,17 @@
 const pokeApi = {};
 
+pokeApi.getPokemonDetail = (pokemon) =>{
+  return fetch(pokemon.url).then((response) => response.json())
+}
+
 pokeApi.getPokemons = function (offset = 0, limit = 5) {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+
   return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => pokemons.map((pokemon) => fetch(pokemon.url).json()))
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
     .then((detailRequest) => Promise.all(detailRequest))
-    .then((pokemonsDetail) => {
-      debugger
-      console.log(pokemonsDetail);
-    })
-};
-
-Promise.all([
-  fetch('https://pokeapi.co/api/v2/pokemon/1'),
-  fetch('https://pokeapi.co/api/v2/pokemon/2'),
-  fetch('https://pokeapi.co/api/v2/pokemon/3'),
-  fetch('https://pokeapi.co/api/v2/pokemon/4')
-]).then((results) => {
-  console.log(results);
-})
+    .then((pokemonsDetails) => pokemonsDetails)
+}

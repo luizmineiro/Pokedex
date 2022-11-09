04568ea2b2dd429b041ca3786a8e0c5d6091a70d
luizmineiro <jluizmineiro@gmail.com>
2022-11-09 12:16:34 -0300

    cncluindo projeto

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -22,7 +22,7 @@ pokeApi.getPokemonDetail = (pokemon) =>{
           .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = function (offset = 0, limit = 10) {
+pokeApi.getPokemons = function (offset = 0, limit = 5) {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 
